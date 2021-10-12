239363c93632f940c42ea566d4a2b0b8eccec866
CleoY <77142560+CleoY@users.noreply.github.com>
2021-10-12 16:23:46 -0700

    Tested malloc() and realloc() functions with anotherTry.c (which should NOT be merged with main.c but should be DELETED later). Tested to see if realloc() will copy old data to new memory block (success); if realloc() with size = count*sizeof(ContactInfo*) allocates the correct amount of data (mostly success; reallocates even # times ContactInfo*); and if decreasing size of list will delete last entry in list (fail; but did not experiment too much with deletion).

--- anotherTry.c
+++ anotherTry.c
@@ -92,7 +92,7 @@ int main(){
     printf("List size BEFORE w/ &: %lu\n", sizeof(&address_book.list));
     printf("List size BEFORE w/*: %lu\n", sizeof(*address_book.list));
     printf("List size w/ malloc_size: %zu\n", malloc_size(address_book.list));
-    address_book.list = malloc(2*sizeof(ContactInfo*));
+    address_book.list = malloc(0);
     printf("List address: %p\n", address_book.list);
     printf("List size AFTER: %lu\n", sizeof(address_book.list));
     printf("List size AFTER w/ &: %lu\n", sizeof(&address_book.list));
@@ -113,7 +113,7 @@ int main(){
     strcpy(contact2.email_addresses[0], "bleh@");
     strcpy(contact2.email_addresses[1], "kek@");
     
-    address_book.list[0] = contact1; //or use *contact1? but * DEREFERENCES contact1
+    address_book.list[0] = contact1;
     address_book.list[1] = contact2;
     printf("Name: %s\n", address_book.list[0].name[0]);
     printf("Phone 1: %s\n", address_book.list[0].phone_numbers[0]);
@@ -126,6 +126,56 @@ int main(){
     printf("Phone 2: %s\n", address_book.list[1].phone_numbers[1]);
     printf("Email 1: %s\n", address_book.list[1].email_addresses[0]);
     printf("Email 2: %s\n", address_book.list[1].email_addresses[1]);
+
+
+    //realloc copies data?? and realloc changes mem size to count * sizeof(CIptr)
+    address_book.count = 3;
+    address_book.list = realloc(address_book.list, (address_book.count)*sizeof(ContactInfo*));
+    printf("Realloc list with malloc_size: %zu\n", malloc_size(address_book.list));
+    
+    //temp works to check null? temp erases list after function finishes?
+
+    ContactInfo contact3;
+    strcpy(contact3.name[0], "Egg");
+    strcpy(contact3.phone_numbers[0], "000");
+    strcpy(contact3.phone_numbers[1], "811");
+    strcpy(contact3.phone_numbers[2], "4000");
+    strcpy(contact3.phone_numbers[3], "811");
+    strcpy(contact3.phone_numbers[4], "789");
+    strcpy(contact3.email_addresses[0], "lic@");
+    strcpy(contact3.email_addresses[1], "ori@");
+    strcpy(contact3.email_addresses[2], "wew@");
+    strcpy(contact3.email_addresses[3], "yiu@");
+    strcpy(contact3.email_addresses[4], "kek@");
+    
+    address_book.list[address_book.count-1] = contact3;
+    printf("Name: %s\n", address_book.list[0].name[0]);
+    printf("Phone 1: %s\n", address_book.list[0].phone_numbers[0]);
+    printf("Phone 2: %s\n", address_book.list[0].phone_numbers[1]);
+    printf("Email 1: %s\n", address_book.list[0].email_addresses[0]);
+    printf("Email 2: %s\n", address_book.list[0].email_addresses[1]);
+
+    printf("Name: %s\n", address_book.list[1].name[0]);
+    printf("Phone 1: %s\n", address_book.list[1].phone_numbers[0]);
+    printf("Phone 2: %s\n", address_book.list[1].phone_numbers[1]);
+    printf("Email 1: %s\n", address_book.list[1].email_addresses[0]);
+    printf("Email 2: %s\n", address_book.list[1].email_addresses[1]);
+
+    //deleting a contact at the end of list by decreasing reallocated size
+    address_book.list = realloc(address_book.list, (2)*sizeof(ContactInfo*));
+    printf("Realloc to delete: %zu\n", malloc_size(address_book.list));
+
+    printf("Name: %s\n", address_book.list[2].name[0]);
+    printf("Phone 1: %s\n", address_book.list[2].phone_numbers[0]);
+    printf("Phone 2: %s\n", address_book.list[2].phone_numbers[1]);
+    printf("Phone 3: %s\n", address_book.list[2].phone_numbers[2]);
+    printf("Phone 4: %s\n", address_book.list[2].phone_numbers[3]);
+    printf("Phone 5: %s\n", address_book.list[2].phone_numbers[4]);
+    printf("Email 1: %s\n", address_book.list[2].email_addresses[0]);
+    printf("Email 2: %s\n", address_book.list[2].email_addresses[1]);
+    printf("Email 3: %s\n", address_book.list[2].email_addresses[2]);
+    printf("Email 4: %s\n", address_book.list[2].email_addresses[3]);
+    printf("Email 5: %s\n", address_book.list[2].email_addresses[4]);
 
     free(address_book.list);
 
