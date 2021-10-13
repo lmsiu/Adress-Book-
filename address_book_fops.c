39995c2b512da744c94b0eccdfce25b0017910fc
Makarius Salib <misalib@cpp.edu>
2021-10-13 12:22:48 -0700

    Defines load_file() to populate an address book

--- address_book_fops.c
+++ address_book_fops.c
@@ -10,24 +10,71 @@
 
 Status load_file(AddressBook *address_book)
 {
-	int ret;
+	//Max possible length for a contact, including commas
+   unsigned int len = (NAME_COUNT * NAME_LEN) + (PHONE_NUMBER_COUNT * NUMBER_LEN) + (EMAIL_ID_COUNT * EMAIL_ID_LEN) + (EMAIL_ID_COUNT + PHONE_NUMBER_COUNT + NAME_COUNT);
+   char contactBuff[len]; 
 
-	/* 
-	 * Check for file existance
-	 */
+   //Try opening the file for reading
+   FILE * fp = fopen(DEFAULT_FILE, "r");
 
-	if (ret == 0)
+	if (!fp) //Make sure the pointer is not null
 	{
-		/* 
-		 * Do the neccessary step to open the file
-		 * Do error handling
-		 */ 
+      address_book->fp = fp;
+      address_book->list = malloc(0);
+      if (!list)
+      {
+         printf("Not able to allocate any memory.");
+         return e_fail;
+      }
+      ContactInfo * currPerson = address_book->list;
+      unsigned int count = 0;
+      while (EOF != fscanf(fp, "%s", contactBuff)) //Input until end of file
+      {
+         ContactInfo * tempPtr = realloc(address_book->list, sizeof(ContactInfo));
+         if (!tempPtr)
+         {
+            printf("Not able to allocate more memory. %i people loaded.", count);
+            return e_fail;
+         }
+         count++;
+         address_book->list = tempPtr;                //Reassign list to new people
+         currPerson = address_book->list + count - 1; //Start a new person
+         unsigned int currChar = 0;                   //Read through the string one character at a time
+         while (contactBuff[currChar] != ',')         //Read name
+         {
+            currPerson.name[currChar] = contactBuff[currChar];
+            currChar++;
+         }
+         currChar++;                //Move past comma
+         for (int phone = 0; phone < PHONE_NUMBER_COUNT; phone++) //Read each phone number
+         {
+            while (contactBuff[currChar] != ',')
+            {
+               currPerson.phone_numbers[phone][currChar] = contactBuff[currChar];
+               currChar++
+            }
+            currChar++;
+         }
+         for (int email = 0; email < EMAIL_ID_COUNT; email++)  //Read each email
+         {
+            while (contactBuff[currChar] != ',')
+            {
+               currPerson.email_addresses[email][currChar] = contactBuff[currChar];
+               currChar++
+            }
+            currChar++;
+         }
+         currPerson.si_no = count;  //Assign a serial number
+         address_book->count = count; //Update the count
+      }
 	}
-	else
+	else  //No file found
 	{
-		/* Create a file for adding entries */
+      fp = fopen(DEFAULT_FILE, "w"); //Create a file
+      address_book->count = 0;
+      address_book->fp = fp;
 	}
-
+   fclose(fp);
 	return e_success;
 }
 
