f7ab149d1d73b05221eca50a7caa53c7e80f1e3b
CleoY <77142560+CleoY@users.noreply.github.com>
2021-10-13 18:54:50 -0700

    Updated save_file() function in address_book_fops.c file based on save_file() function version in saveFileAttempt.c.

--- address_book_fops.c
+++ address_book_fops.c
@@ -44,6 +44,18 @@ Status save_file(AddressBook *address_book)
 		return e_fail;
 	}
 
+	//ContactInfo* tempListPtr = address_book->list;
+	for(int contactNum = 0; contactNum < address_book->count; contactNum++){
+		fprintf(address_book->fp, "%s,",address_book->list[contactNum].name[0]);
+		for(int phoneNum = 0; phoneNum < PHONE_NUMBER_COUNT; phoneNum++){
+			fprintf(address_book->fp,"%s,",address_book->list[contactNum].phone_numbers[phoneNum]);
+		}
+		for(int emailCount = 0; emailCount < EMAIL_ID_COUNT; emailCount++){
+			fprintf(address_book->fp, "%s,",address_book->list[contactNum].email_addresses[emailCount]);
+		}
+		fprintf(address_book->fp, "%d\n", address_book->list[contactNum].si_no);
+	}
+
 	/* 
 	 * Add the logic to save the file
 	 * Make sure to do error handling
